5d918ed00ecf25970cc6eaf4430022c6fbb9f160
Pallalallo <59975252+Pallalallo@users.noreply.github.com>
2020-04-03 21:04:21 +0200

    Update defer.js

--- defer.js
+++ defer.js
@@ -1,1 +1,8 @@
-
+Function.prototype.defer = function (ms) {
+    return setTimeout(this, ms);
+}
+function printNumb() {
+    console.log(ms / 1000, 'seconds');
+}
+let ms = 3000;
+printNumb.defer(3000)
